0f3d7d83c4cd9d3934819db975b62c5e746283da
MalithSrineth <malithsrineth@gmail.com>
2023-07-07 20:48:46 +0530

    Started

---
+++ src/Theatre.java
@@ -0,0 +1,14 @@
+import java.util.Arrays;
+
+public class Theatre {
+    public static void main(String[] args) {
+        System.out.println("Welcome to the New Theatre");
+        int [] firstRow = new int[12];
+        int [] secondRow = new int[16];
+        int [] thirdRow = new int[20];
+
+        Arrays.fill(firstRow, 0);
+        Arrays.fill(secondRow, 0);
+        Arrays.fill(thirdRow, 0);
+    }
+}
